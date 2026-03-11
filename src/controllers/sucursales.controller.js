6f51a51d0bae86fc6692eaf1a7bec55170c8ad31
CrixL94 <96554800+CrixL94@users.noreply.github.com>
2026-03-11 14:13:12 -0600

    CRUD Sucursales

--- src/controllers/sucursales.controller.js
+++ src/controllers/sucursales.controller.js
@@ -5,9 +5,54 @@ exports.listarSucursales = async (req, res) => {
   try {
     const pool = await poolPromise;
     const result = await pool.request()
-      .query('select IdSucursal, Nombre as NombreSucursal from dbo.sucursales');
+      .query('select IdSucursal, Nombre as NombreSucursal from dbo.sucursales where IdEstado = 1');
     res.json(result.recordset);
   } catch (error) {
     res.status(500).json({ error: error.message });
+  }
+};
+
+exports.listarSucursalesVista = async (req, res) => {
+  try {
+    const pool = await poolPromise;
+    const result = await pool.request()
+      .query('SELECT * FROM dbo.vw_Sucursales where IdEstado = 1 ORDER BY FechaCreacion DESC');
+    
+    res.json(result.recordset);
+  } catch (error) {
+    res.status(500).json({ 
+      success: false, 
+      error: error.message 
+    });
+  }
+};
+
+exports.gestionarSucursales = async (req, res) => {
+  const { Accion, IdSucursal, Nombre, Direccion, IdUsuario } = req.body;
+
+  try {
+    const pool = await poolPromise;
+    const result = await pool.request()
+      .input('JsonData', sql.NVarChar(sql.MAX), JSON.stringify({
+        Accion,
+        IdSucursal,
+        Nombre,
+        Direccion,
+        IdUsuario
+      }))
+      .execute('sp_CRUDSucursales');
+
+    const respuesta = result.recordset[0];
+
+    res.json({
+      success: respuesta.Success === 1,
+      message: respuesta.Message
+    });
+
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      error: error.message
+    });
   }
 };
--- src/routes/sucursales.routes.js
+++ src/routes/sucursales.routes.js
@@ -5,4 +5,8 @@ const sucursalesController = require('../controllers/sucursales.controller')
 // Obtener todas las sucursales
 router.get('/', sucursalesController.listarSucursales);
 
+router.get('/view', sucursalesController.listarSucursalesVista);
+
+router.post('/gestionar', sucursalesController.gestionarSucursales);
+
 module.exports = router;
